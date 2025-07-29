a5be80ede472c78dab2278a10bb69ca7b1655d69
Anusha-star-dot <Anusha-star-dot@users.noreply.github.com>
2025-07-29 17:57:50 +0530

    Add pytest test for backend and fix GitHub Action

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,6 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,4 +8,7 @@ def home():
     return "Hello from Backend Flask API!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    # Run only if not in CI
+    if os.getenv("CI") != "true":
+        app.run(host='0.0.0.0', port=5000)
+
